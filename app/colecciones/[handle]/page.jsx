54724b699e98bf392f201c133ad3fca227e065aa
Martin Maldonado <martinmaldonado592@gmail.com>
2026-08-12 17:52:01 -0500

    fix: corregir ruta relativa de importación de MainLayout para el build de Vercel

--- app/colecciones/[handle]/page.jsx
+++ app/colecciones/[handle]/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MainLayout } from '../../src/MainLayout';
+import { MainLayout } from '../../../src/MainLayout';
 
 export default function CollectionRoute() {
   return <MainLayout />;
